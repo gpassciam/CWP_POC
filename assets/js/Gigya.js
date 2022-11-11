3e1ae9a63cb059438e3deab40425c4e48f8c7f64
ciampoc <104811739+gpassciam@users.noreply.github.com>
2022-11-11 10:04:51 +0530

    u

---
+++ assets/js/Gigya.js
@@ -0,0 +1,24 @@
+﻿
+function load() {
+    //Moesif CORS
+    
+        $("#header").load("Header.html");
+        $("#footer").load("Footer.html");
+
+customLangParams = {
+
+        this_field_is_required: 'Please enter %fieldname'
+    };
+    
+        gigya.accounts.showScreenSet({
+            //screenSet: 'CWP-DEV-RegistrationLogin',
+            screenSet: 'CWP-DEV-Login-ext',
+            startScreen: 'gigya-login-screen',
+            customLang: customLangParams,
+            containerID: 'LoginScreen'
+            
+        });
+}
+
+
+    
